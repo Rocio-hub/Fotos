42bfaf6ea4c066702e41dec07e460a197d383bad
Rocio-hub <roci0055@easv365.dk>
2022-07-05 11:42:55 +0200

    .

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FotoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
